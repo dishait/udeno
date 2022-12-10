a2d448d5de7a9f4db399d4c9e47893877af1679f
markthree <1801982702@qq.com>
2022-12-10 22:14:40 +0800

    feat: pure import support

--- src/normalize.ts
+++ src/normalize.ts
@@ -73,7 +73,7 @@ export function createReplace(
 		return content.replace(
 			code,
 			code.replace(
-				new RegExp(`(?<=from.*['"])${specifier}`),
+				new RegExp(`${specifier}(?=['"])`),
 				replaceValue
 			)
 		)
