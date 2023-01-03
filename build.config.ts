126941ebac7c3a59b079a9fb6e379b466cebc384
markthree <1801982702@qq.com>
2023-01-03 16:08:17 +0800

    chore: more streamlined configuration

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,5 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-	failOnWarn: false, // cpy is esm-only modules，so we need to package it as an inline dependency
-	declaration: true,
-	entries: ['src/cli', 'src/index'],
-	rollup: {
-		emitCJS: true
-	}
+	failOnWarn: false // cpy is esm-only modules，so we need to package it as an inline dependency
 })
